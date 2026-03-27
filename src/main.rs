06cd90b8d9c8ff47b0448aae4476f0709d374b5b
0xA672 <cero0xA672@user.noreply.gitee.com>
2026-03-28 07:30:10 +0800

    owo

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
-use std::os;
+use std::env;
+use std::io::{BufRead, BufReader, Error, Write};
+use std::process::Command;
+use walkdir::WalkDir;
+use std::path::PathBuf;
+
+
+fn pwd() -> std::io::Result<PathBuf> {
+    env::current_dir()
+}
+
+fn scandir() -> std::io::Result<Vec<PathBuf>> {
+    let cwd = pwd()?;
+    let mut pathofzigzon = Vec::new();
+    for i in WalkDir::new(cwd) {
+        let i = i.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;///turn walkdir::Error into ErrorKind::Other
+        if i.file_name() == "build.zig.zon" {
+            pathofzigzon.push(i.into_path());
+        }
+    }
+    Ok(pathofzigzon)
+}
+
+fn scanzon() {
+    let path = scandir();
+}
+
+fn main(){
+
+}
